0e57c5955f9e9dde2ad5256fed34222e8aacef87
TateLe1 <tle42@huskers.unl.edu>
2020-08-25 04:37:15 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Tate Le
+ * Date:    2020
+ * 
+ * A simple hello world program 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    printf("Hello World!\n");
+    
+    return 0;
+    
+}
